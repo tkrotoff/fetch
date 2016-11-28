f978a133bdfa8cdaa2f9b400a24e6b624ddf235e
Tanguy Krotoff <tkrotoff@gmail.com>
2016-11-28 01:43:45 +0100

    First implementation

---
+++ src/Http.ts
@@ -0,0 +1,92 @@
+﻿import 'whatwg-fetch';
+import HttpStatus from './HttpStatus';
+
+type Headers = {[index: string]: string};
+
+const JSON_HEADERS: Headers = {
+  'Accept': 'application/json',
+  'Content-Type': 'application/json'
+};
+
+export function postJSON<T>(url: string, body: any) {
+  return fetch(url, {
+      method: 'POST',
+      headers: JSON_HEADERS,
+      body: JSON.stringify(body)
+    })
+    .then(debugThrottle)
+    .then(checkStatus)
+    .then(response => parseJSON<T>(response));
+}
+
+export function putJSON<T>(url: string, body: any) {
+  return fetch(url, {
+      method: 'PUT',
+      headers: JSON_HEADERS,
+      body: JSON.stringify(body)
+    })
+    .then(debugThrottle)
+    .then(checkStatus)
+    .then(response => parseJSON<T>(response));
+}
+
+export function getJSON<T>(url: string, headers?: Headers) {
+  return fetch(url, {
+      headers: Object.assign(JSON_HEADERS, headers)
+      // FIXME error TS2698: Spread types may only be created from object types
+      //headers: { ...JSON_HEADERS, ...headers }
+    })
+    .then(debugThrottle)
+    .then(checkStatus)
+    .then(response => parseJSON<T>(response));
+}
+
+export function deleteJSON<T>(url: string) {
+  return fetch(url, {
+      method: 'DELETE',
+      headers: JSON_HEADERS
+    })
+    .then(debugThrottle)
+    .then(checkStatus)
+    .then(response => parseJSON<T>(response));
+}
+
+class HttpError extends Error {
+  response: Response;
+}
+
+// See Handling HTTP error statuses https://github.com/github/fetch#handling-http-error-statuses
+function checkStatus(response: Response) {
+  if (response.status >= HttpStatus.OK_200 && response.status < HttpStatus.MultipleChoices_300) {
+    return response;
+  } else {
+    const error = new HttpError(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
+function debugThrottle(response: Response) {
+  return response;
+}
+
+/*
+function debugThrottle(response: Response) {
+  function random(min: number, max: number) {
+    return Math.floor(Math.random() * max) + min;
+  }
+
+  return new Promise(resolve =>
+    setTimeout(resolve, random(1000, 5000))
+  ).then(() => response);
+}
+*/
+
+function parseJSON<T>(response: Response) {
+  // An empty string is not valid JSON so JSON.parse() fails
+  if (response.status !== HttpStatus.NoContent_204) {
+    return response.json<T>();
+  } else {
+    return new Promise<T>((resolve, reject) => resolve());
+  }
+}
---
+++ src/HttpStatus.ts
@@ -0,0 +1,30 @@
+/**
+ * List of HTTP status codes.
+ *
+ * See List of HTTP status codes http://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+ * See Rails HTTP Status Code to Symbol Mapping http://www.codyfauser.com/2008/7/4/rails-http-status-code-to-symbol-mapping
+ */
+enum HttpStatus {
+  /* 1xx Informational */
+
+  /* 2xx Success */
+  OK_200 = 200,
+  Created_201 = 201,
+  NoContent_204 = 204,
+
+  /* 3xx Redirection */
+  MultipleChoices_300 = 300,
+  MovedPermanently_301 = 301,
+
+  /* 4xx Client Error */
+  BadRequest_400 = 400,
+  Unauthorized_401 = 401,
+  NotFound_404 = 404,
+  Conflict_409 = 409,
+  UnprocessableEntity_422 = 422,
+
+  /* 5xx Server Error */
+  InternalServerError_500 = 500
+}
+
+export default HttpStatus;
