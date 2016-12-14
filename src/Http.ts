88fae09396ba5bbfe06deba9413e2a665df4bb96
Tanguy Krotoff <tkrotoff@gmail.com>
2016-12-14 16:35:15 +0100

    Fix subclassing Error when generating ES5 JavaScript

--- src/Http.ts
+++ src/Http.ts
@@ -52,6 +52,14 @@ export function deleteJSON<T>(url: string) {
 }
 
 class HttpError extends Error {
+  constructor(message?: string) {
+    super(message);
+
+    // See Extending built-ins like Error, Array, and Map may no longer work
+    // https://github.com/Microsoft/TypeScript/wiki/Breaking-Changes#extending-built-ins-like-error-array-and-map-may-no-longer-work
+    Object.setPrototypeOf(this, HttpError.prototype);
+  }
+
   response: Response;
 }
 
