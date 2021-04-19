4331ed0af62b88bb33257be791e38b827d4c91be
Tanguy Krotoff <tkrotoff@gmail.com>
2021-04-20 01:41:31 +0200

    Simplify webpack.config.js

--- examples/web/webpack.config.js
+++ examples/web/webpack.config.js
@@ -8,8 +8,7 @@ const config = {
   entry: './index.ts',
 
   output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].js'
+    path: path.join(__dirname, 'build')
   },
 
   resolve: {
@@ -18,15 +17,8 @@ const config = {
 
   module: {
     rules: [
-      {
-        test: /\.(js|ts)$/,
-
-        // [Babel should not transpile core-js](https://github.com/zloirock/core-js/issues/514#issuecomment-476533317)
-        exclude: /\/core-js/,
-
-        loader: 'babel-loader'
-      },
-      { test: /\.html$/, loader: 'file-loader', options: { name: '[path][name].[ext]' } }
+      { test: /\.ts$/, loader: 'babel-loader' },
+      { test: /\.html$/, type: 'asset/resource', generator: { filename: '[name][ext]' } }
     ]
   }
 };
