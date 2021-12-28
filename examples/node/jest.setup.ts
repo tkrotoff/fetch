e9d5e0fe49a2f5243e74e48e69a5c9dd99d4a1ee
Tanguy Krotoff <tkrotoff@gmail.com>
2021-12-28 23:33:48 +0100

    No need to catch unhandledRejection with Node.js 16

--- examples/node/jest.setup.ts
+++ examples/node/jest.setup.ts
@@ -6,20 +6,6 @@ import * as nodeFetch from 'node-fetch';
 // eslint-disable-next-line no-console
 console.assert = assert;
 
-// [Event: 'unhandledRejection'](https://nodejs.org/api/process.html#process_event_unhandledrejection)
-// [Bluebird Error management configuration](http://bluebirdjs.com/docs/api/error-management-configuration.html)
-//
-// Node.js error:
-// (node:38141) UnhandledPromiseRejectionWarning: Unhandled promise rejection.
-// This error originated either by throwing inside of an async function without a catch block,
-// or by rejecting a promise which was not handled with .catch(). (rejection id: 4)
-// (node:38141) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated.
-// In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
-//
-process.on('unhandledRejection', (reason: Error | any, _promise: Promise<any>) => {
-  throw reason;
-});
-
 globalThis.Headers = nodeFetch.Headers as any;
 globalThis.Response = nodeFetch.Response as any;
 
