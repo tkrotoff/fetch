6d96b6c120cbada44b52f0a110719b0a6c9e25f3
Tanguy Krotoff <tkrotoff@gmail.com>
2022-11-30 01:49:07 +0100

    Fix example tests with Node.js 18 thx to jest.mock

--- examples/node/requests.test.ts
+++ examples/node/requests.test.ts
@@ -1,4 +1,13 @@
-import * as Http from '@tkrotoff/fetch';
+import {
+  createJSONResponsePromise,
+  createResponsePromise,
+  del,
+  get,
+  HttpStatus,
+  Init,
+  postJSON,
+  ResponsePromiseWithBodyMethods
+} from '@tkrotoff/fetch';
 
 import {
   abortRequestExample,
@@ -10,9 +19,23 @@ import {
   postJSON201CreatedExample
 } from './requests';
 
+beforeEach(() => jest.resetAllMocks());
+
+jest.mock('@tkrotoff/fetch', () => ({
+  ...jest.requireActual('@tkrotoff/fetch'),
+  get: jest.fn(),
+  //post: jest.fn(),
+  postJSON: jest.fn(),
+  //put: jest.fn(),
+  //putJSON: jest.fn(),
+  //patch: jest.fn(),
+  //patchJSON: jest.fn(),
+  del: jest.fn()
+}));
+
 test('get200OKExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockImplementation(() =>
-    Http.createJSONResponsePromise({
+  jest.mocked(get).mockImplementation(() =>
+    createJSONResponsePromise({
       userId: 1,
       id: 1,
       title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
@@ -21,15 +44,13 @@ test('get200OKExample()', async () => {
   );
 
   await get200OKExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
 });
 
 test('postJSON201CreatedExample()', async () => {
-  const mock = jest.spyOn(Http, 'postJSON').mockImplementation(() =>
-    Http.createJSONResponsePromise({
+  jest.mocked(postJSON).mockImplementation(() =>
+    createJSONResponsePromise({
       id: 101,
       title: 'foo',
       body: 'bar',
@@ -38,64 +59,54 @@ test('postJSON201CreatedExample()', async () => {
   );
 
   await postJSON201CreatedExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts', {
+  expect(postJSON).toHaveBeenCalledTimes(1);
+  expect(postJSON).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts', {
     body: 'bar',
     title: 'foo',
     userId: 1
   });
-
-  mock.mockRestore();
 });
 
 test('del200OKExample()', async () => {
-  const mock = jest.spyOn(Http, 'del').mockImplementation(() => Http.createJSONResponsePromise({}));
+  jest.mocked(del).mockImplementation(() => createJSONResponsePromise({}));
 
   await del200OKExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
-
-  mock.mockRestore();
+  expect(del).toHaveBeenCalledTimes(1);
+  expect(del).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
 });
 
 test('get404NotFoundExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockImplementation(() =>
-    Http.createResponsePromise('404 Not Found', {
-      status: Http.HttpStatus._404_NotFound,
+  jest.mocked(get).mockImplementation(() =>
+    createResponsePromise('404 Not Found', {
+      status: HttpStatus._404_NotFound,
       statusText: 'Not Found'
     })
   );
 
   await get404NotFoundExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://httpstat.us/404/cors');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://httpstat.us/404/cors');
 });
 
 test('get500InternalServerErrorExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockImplementation(() =>
-    Http.createResponsePromise('500 Internal Server Error', {
-      status: Http.HttpStatus._500_InternalServerError,
+  jest.mocked(get).mockImplementation(() =>
+    createResponsePromise('500 Internal Server Error', {
+      status: HttpStatus._500_InternalServerError,
       statusText: 'Internal Server Error'
     })
   );
 
   await get500InternalServerErrorExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://httpstat.us/500/cors');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://httpstat.us/500/cors');
 });
 
 test('getCorsBlockedExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockRejectedValue(new TypeError('Failed to fetch'));
+  jest.mocked(get).mockRejectedValue(new TypeError('Failed to fetch'));
 
   await getCorsBlockedExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://postman-echo.com/get?foo1=bar1&foo2=bar2');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://postman-echo.com/get?foo1=bar1&foo2=bar2');
 });
 
 test('abortRequestExample()', async () => {
@@ -104,31 +115,27 @@ test('abortRequestExample()', async () => {
   const abortError = new Error('The operation was aborted.');
   abortError.name = 'AbortError';
 
-  const mock = jest
-    .spyOn(Http, 'get')
-    .mockImplementation((_input: RequestInfo, init: Http.Init) => {
-      // Mock aborted request
-      // https://github.com/github/fetch/blob/v3.4.1/fetch.js#L497
-      const response = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (init.signal && init.signal.aborted) {
-            reject(abortError);
-          }
-          resolve('**********');
-        }, 600);
-      });
-
-      return response as Http.ResponsePromiseWithBodyMethods;
+  jest.mocked(get).mockImplementation((_input, init) => {
+    // Mock aborted request
+    // https://github.com/github/fetch/blob/v3.4.1/fetch.js#L497
+    const response = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (init!.signal && init!.signal.aborted) {
+          reject(abortError);
+        }
+        resolve('**********');
+      }, 600);
     });
 
+    return response as ResponsePromiseWithBodyMethods;
+  });
+
   await abortRequestExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith(
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith(
     'https://httpbin.org/drip?duration=2&numbytes=10&code=200&delay=2',
     {
       signal: expect.any(AbortSignal)
     }
   );
-
-  mock.mockRestore();
 });
--- examples/web/requests.test.ts
+++ examples/web/requests.test.ts
@@ -1,4 +1,13 @@
-import * as Http from '@tkrotoff/fetch';
+import {
+  createJSONResponsePromise,
+  createResponsePromise,
+  del,
+  get,
+  HttpStatus,
+  post,
+  postJSON,
+  ResponsePromiseWithBodyMethods
+} from '@tkrotoff/fetch';
 
 import {
   abortRequestExample,
@@ -12,9 +21,23 @@ import {
   uploadFilesExample
 } from './requests';
 
+beforeEach(() => jest.resetAllMocks());
+
+jest.mock('@tkrotoff/fetch', () => ({
+  ...jest.requireActual('@tkrotoff/fetch'),
+  get: jest.fn(),
+  post: jest.fn(),
+  postJSON: jest.fn(),
+  //put: jest.fn(),
+  //putJSON: jest.fn(),
+  //patch: jest.fn(),
+  //patchJSON: jest.fn(),
+  del: jest.fn()
+}));
+
 test('get200OKExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockImplementation(() =>
-    Http.createJSONResponsePromise({
+  jest.mocked(get).mockImplementation(() =>
+    createJSONResponsePromise({
       userId: 1,
       id: 1,
       title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
@@ -23,15 +46,13 @@ test('get200OKExample()', async () => {
   );
 
   await get200OKExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
 });
 
 test('postJSON201CreatedExample()', async () => {
-  const mock = jest.spyOn(Http, 'postJSON').mockImplementation(() =>
-    Http.createJSONResponsePromise({
+  jest.mocked(postJSON).mockImplementation(() =>
+    createJSONResponsePromise({
       id: 101,
       title: 'foo',
       body: 'bar',
@@ -40,69 +61,59 @@ test('postJSON201CreatedExample()', async () => {
   );
 
   await postJSON201CreatedExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts', {
+  expect(postJSON).toHaveBeenCalledTimes(1);
+  expect(postJSON).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts', {
     body: 'bar',
     title: 'foo',
     userId: 1
   });
-
-  mock.mockRestore();
 });
 
 test('del200OKExample()', async () => {
-  const mock = jest.spyOn(Http, 'del').mockImplementation(() => Http.createJSONResponsePromise({}));
+  jest.mocked(del).mockImplementation(() => createJSONResponsePromise({}));
 
   await del200OKExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
-
-  mock.mockRestore();
+  expect(del).toHaveBeenCalledTimes(1);
+  expect(del).toHaveBeenCalledWith('https://jsonplaceholder.typicode.com/posts/1');
 });
 
 test('get404NotFoundExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockImplementation(() =>
-    Http.createResponsePromise('404 Not Found', {
-      status: Http.HttpStatus._404_NotFound,
+  jest.mocked(get).mockImplementation(() =>
+    createResponsePromise('404 Not Found', {
+      status: HttpStatus._404_NotFound,
       statusText: 'Not Found'
     })
   );
 
   await get404NotFoundExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://httpstat.us/404/cors');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://httpstat.us/404/cors');
 });
 
 test('get500InternalServerErrorExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockImplementation(() =>
-    Http.createResponsePromise('500 Internal Server Error', {
-      status: Http.HttpStatus._500_InternalServerError,
+  jest.mocked(get).mockImplementation(() =>
+    createResponsePromise('500 Internal Server Error', {
+      status: HttpStatus._500_InternalServerError,
       statusText: 'Internal Server Error'
     })
   );
 
   await get500InternalServerErrorExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://httpstat.us/500/cors');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://httpstat.us/500/cors');
 });
 
 test('getCorsBlockedExample()', async () => {
-  const mock = jest.spyOn(Http, 'get').mockRejectedValue(new TypeError('Failed to fetch'));
+  jest.mocked(get).mockRejectedValue(new TypeError('Failed to fetch'));
 
   await getCorsBlockedExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://postman-echo.com/get?foo1=bar1&foo2=bar2');
-
-  mock.mockRestore();
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith('https://postman-echo.com/get?foo1=bar1&foo2=bar2');
 });
 
 test('uploadFilesExample()', async () => {
-  const mock = jest.spyOn(Http, 'post').mockImplementation(() =>
-    Http.createJSONResponsePromise({
+  jest.mocked(post).mockImplementation(() =>
+    createJSONResponsePromise({
       files: { file0: 'file0Content', file1: 'file1Content' }
     })
   );
@@ -111,42 +122,36 @@ test('uploadFilesExample()', async () => {
   const file1 = new File(['file1Content'], 'file1', { type: 'text/plain' });
 
   await uploadFilesExample([file0, file1] as unknown as FileList);
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith('https://httpbin.org/anything', expect.any(FormData));
-
-  mock.mockRestore();
+  expect(post).toHaveBeenCalledTimes(1);
+  expect(post).toHaveBeenCalledWith('https://httpbin.org/anything', expect.any(FormData));
 });
 
 test('abortRequestExample()', async () => {
   const abortError = new DOMException('The user aborted a request.', 'AbortError');
 
-  const mock = jest
-    .spyOn(Http, 'get')
-    .mockImplementation((_input: RequestInfo, init: Http.Init) => {
-      // Mock aborted request
-      // https://github.com/github/fetch/blob/v3.4.1/fetch.js#L497
-      const response = new Promise((resolve, reject) => {
-        setTimeout(() => {
-          if (init.signal && init.signal.aborted) {
-            reject(abortError);
-          }
-          resolve('**********');
-        }, 600);
-      });
-
-      return response as Http.ResponsePromiseWithBodyMethods;
+  jest.mocked(get).mockImplementation((_input, init) => {
+    // Mock aborted request
+    // https://github.com/github/fetch/blob/v3.4.1/fetch.js#L497
+    const response = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (init!.signal && init!.signal.aborted) {
+          reject(abortError);
+        }
+        resolve('**********');
+      }, 600);
     });
 
+    return response as ResponsePromiseWithBodyMethods;
+  });
+
   await abortRequestExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith(
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith(
     'https://httpbin.org/drip?duration=2&numbytes=10&code=200&delay=2',
     {
       signal: expect.any(AbortSignal)
     }
   );
-
-  mock.mockRestore();
 });
 
 // FIXME jsdom does not support Blob.stream https://github.com/jsdom/jsdom/issues/2555
@@ -164,8 +169,8 @@ test.skip('downloadProgressExample()', async () => {
   const blob = new Blob([content.buffer]);
   const stream = blob.stream();
 
-  const mock = jest.spyOn(Http, 'get').mockImplementation(() =>
-    Http.createResponsePromise(stream, {
+  jest.mocked(get).mockImplementation(() =>
+    createResponsePromise(stream, {
       headers: {
         'content-length': blob.size.toString()
       }
@@ -173,10 +178,8 @@ test.skip('downloadProgressExample()', async () => {
   );
 
   await downloadProgressExample();
-  expect(mock).toHaveBeenCalledTimes(1);
-  expect(mock).toHaveBeenCalledWith(
+  expect(get).toHaveBeenCalledTimes(1);
+  expect(get).toHaveBeenCalledWith(
     'https://fetch-progress.anthum.com/30kbps/images/sunrise-baseline.jpg'
   );
-
-  mock.mockRestore();
 });
